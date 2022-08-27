bc667e57babe1c948fd02aa37d3ca97c0069369c
keg66 <keg@keg66.com>
2022-08-27 10:22:34 +0900

    add main_test.c

---
+++ src/main_test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+#include <rmw_microros/rmw_microros.h>
+
+#include <std_msgs/msg/int32.h>
+
+int main()
+{
+  return 0;
+}
